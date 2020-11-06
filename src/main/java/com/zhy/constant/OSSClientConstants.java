3b643434b46cf36ef5fc975b5bf571210226ebb4
longteng24 <1532712957@qq.com>
2020-11-06 12:15:22 +0800

    项目初始化

--- src/main/java/com/zhy/constant/OSSClientConstants.java
+++ src/main/java/com/zhy/constant/OSSClientConstants.java
@@ -14,7 +14,7 @@ public class OSSClientConstants {
     /**
      * 阿里云API的外网域名
      */
-    public static final String ENDPOINT = "oss-cn-shenzhen.aliyuncs.com";
+    public static final String ENDPOINT = "edu24.oss-cn-beijing.aliyuncs.com";
 
     /**
      * 阿里云API的密钥Access Key ID
@@ -29,7 +29,7 @@ public class OSSClientConstants {
      * 阿里云API的bucket名称
      * 在阿里云上自己创建一个bucket
      */
-    public static final String BACKET_NAME = "zhy-myblog";
+    public static final String BACKET_NAME = "edu24";
 
     /**
      * 阿里云API的文件夹名称
